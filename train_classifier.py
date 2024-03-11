8bf46dd9e46074a25fe785cca581932a4d86d1e0
pmkha <pmkhambhaita@outlook.com>
2024-03-11 22:03:35 +0000

    -

--- train_classifier.py
+++ train_classifier.py
@@ -4,7 +4,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import numpy as np
-
+import tensorflow as tf
 
 data_dict = pickle.load(open('./data.pickle', 'rb'))
 
@@ -27,3 +27,19 @@ print('{}% of samples were classified correctly!'.format(score * 100))
 f = open('model.p', 'wb')
 pickle.dump({'model': model}, f)
 f.close()
+
+
+with open('model.p', 'rb') as f:
+    model_obj = pickle.load(f)
+
+# Recreate the TensorFlow model from the loaded object
+model_tf = tf.keras.models.model_from_json(model_obj.to_json())
+model_tf.set_weights(model_obj.get_weights())
+
+# convert to tflite
+converter = tf.lite.TFLiteConverter.from_keras_model(model_tf)
+tflite_model = converter.convert()
+
+# Save the model.
+with open('model.tflite', 'wb') as f:
+    f.write(tflite_model)
